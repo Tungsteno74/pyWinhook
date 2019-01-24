f2a715ec7665bcaf72c958c35d04fe261fb1b7a3
Tungsteno74 <tungsteno74@yahoo.it>
2019-01-24 03:32:12 +0100

    update setup.py version to 1.6.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ Topic :: System :: Monitoring
 Topic :: Software Development :: Libraries :: Python Modules
 Operating System :: Microsoft :: Windows
 """
-__version__='1.5.2'
+__version__='1.6.0'
 from setuptools import setup, Extension
 
 libs = ['user32']
